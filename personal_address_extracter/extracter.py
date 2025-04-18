62f1e5d2503121de741fdb94d9e152ed9d34c512
Ace_Radom <sichenradomlyu@gmail.com>
2025-04-18 19:04:34 +0800

    extract data from file

--- personal_address_extracter/extracter.py
+++ personal_address_extracter/extracter.py
@@ -20,34 +20,50 @@ RESULT_COORD_X_OFFSET = 1
 RESULT_COORD_Y_OFFSET = 0
 
 class coord:
-    def __init__(self):
+    def __init__(self) -> None:
         self.x = -1
         self.y = -1
         return
 
-    def __init__(self, coord: list):
+    def __init__(self, coord: list) -> None:
         self.x = coord[RESULT_COORD_X_OFFSET]
         self.y = coord[RESULT_COORD_Y_OFFSET]
         return
 
-    def empty(self):
+    def empty(self) -> bool:
         return self.x == -1 and self.y == -1
+    
+# extracted persional data structure in one file 
+class personal_data:
+    def __init__(self) -> None:
+        self.name = ""
+        self.sex = ""
+        self.id = ""
+        self.birth = ""
+        self.addr = ""
+        return
+    
+    def empty(self) -> None:
+        return len(self.name) == 0 and len(self.sex) == 0 and len(self.id) == 0 and len(self.birth) == 0 and len(self.addr) == 0
 
-def perform_ocr(image_path: str):
+def perform_ocr(image_path: str) -> list:
     if not os.path.isfile(image_path):
-        rprint(f"[red]Error: image `{image_path}` doesn't exist.")
+        rprint(f"[red]Error[/]: image `{image_path}` doesn't exist.")
+        return []
+    elif os.path.splitext(image_path)[1] not in [".jpg", ".jpeg", ".png"]:
+        rprint(f"[red]Error[/]: image `{image_path}` has a not supported extension.")
         return []
     
     try:
         result = ocr_worker.ocr(image_path)
     except Exception as ex:
-        rprint(f"[red]Error: failed to perform OCR on image `{image_path}`.[/]\n", ex)
+        rprint(f"[red]Error[/]: failed to perform OCR on image `{image_path}`.\n", ex)
 
     return result[0]
 
-def check_ocr_result(result) -> bool:
+def check_ocr_result(result: list) -> bool:
     if len(result) < 1:
-        rprint("[red]Error: result length less than 1.")
+        rprint("[red]Error[/]: result length less than 1.")
         return False
     else:
         tag_name_found = False
@@ -66,7 +82,7 @@ def check_ocr_result(result) -> bool:
             else: continue
 
         if not (tag_name_found and tag_sex_found and tag_id_found and tag_birth_found and tag_addr_found):
-            rprint("[red]Error: not all necessary tag found.")
+            rprint("[red]Error[/]: not all necessary tag found.")
             return False
     return True
 
@@ -80,7 +96,7 @@ def find_data_with_tag(result, tag: str) -> tuple[str, float]:
             # TODO: orders of coords may be changed in different models, a reorder may be necessary
             break
     if tag_coord_uo.empty():
-        rprint(f"[red]Error: tag `{tag}` not found in OCR result.")
+        rprint(f"[red]Error[/]: tag `{tag}` not found in OCR result.")
         return ("", -1)
     
     closest_line = []
@@ -92,7 +108,7 @@ def find_data_with_tag(result, tag: str) -> tuple[str, float]:
         # skip tag itself
         
         this_coord_uo = coord(line[0][0])
-        if abs(this_coord_uo.y - tag_coord_uo.y) < SAME_LINE_Y_MAX_PIXEL_OFFSET:
+        if abs(this_coord_uo.y - tag_coord_uo.y) < SAME_LINE_Y_MAX_PIXEL_OFFSET and this_coord_uo.x > tag_coord_uo.x:
             if len(closest_line) != 0:
                 if coord(closest_line[0][0]).x <= this_coord_uo.x:
                     continue
@@ -100,10 +116,50 @@ def find_data_with_tag(result, tag: str) -> tuple[str, float]:
             # closest line not empty, judge which one is closer
             closest_line = line
     if len(closest_line) == 0:
-        rprint(f"[red]Error: no data assigned to tag `{tag}` was found.")
+        rprint(f"[red]Error[/]: no data assigned to tag `{tag}` was found.")
         return ("", -1)
     
     return (closest_line[1][0], closest_line[1][1])
+
+def extract_picture_personal_data(image_path: str) -> personal_data:
+    result = perform_ocr(image_path)
+    if not check_ocr_result(result):
+        return {}
+    # illegal ocr result
+
+    data = personal_data()
+
+    data.name, acc = find_data_with_tag(result, TAG_NAME)
+    if acc == -1:
+        return personal_data()
+    elif acc < 0.95:
+        rprint("[yellow]Warning[/]: low accuracy expected on `name`.")
+
+    data.sex, acc = find_data_with_tag(result, TAG_SEX)
+    if acc == -1:
+        return personal_data()
+    elif acc < 0.95:
+        rprint("[yellow]Warning[/]: low accuracy expected on `sex`.")
+
+    data.id, acc = find_data_with_tag(result, TAG_ID)
+    if acc == -1:
+        return personal_data()
+    elif acc < 0.95:
+        rprint("[yellow]Warning[/]: low accuracy expected on `id`.")
+
+    data.birth, acc = find_data_with_tag(result, TAG_BIRTH)
+    if acc == -1:
+        return personal_data()
+    elif acc < 0.95:
+        rprint("[yellow]Warning[/]: low accuracy expected on `birth`.")
+
+    data.addr, acc = find_data_with_tag(result, TAG_ADDR)
+    if acc == -1:
+        return personal_data()
+    elif acc < 0.95:
+        rprint("[yellow]Warning[/]: low accuracy expected on `addr`.")
+
+    return data
 
 def main():
     return
